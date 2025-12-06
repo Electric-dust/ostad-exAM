370bd1d508379f88efcd6776b7e02fd8133ff606
Electric-dust <mahadin8@gmail.com>
2025-12-06 18:52:01 +0600

    app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const router = require('./routes/api');
+
+app.use(express.json());
+app.use('/', router);
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
